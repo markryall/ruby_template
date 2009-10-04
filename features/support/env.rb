d1caf07f18005d6e0f00818a9270edf55bbef127
Mike Bain <mbain@thoughtworks.com>
2009-10-04 09:17:18 +0800

    doh, wrong file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,16 @@
+require 'webrat'  
+require 'spec/expectations'
+
 require File.expand_path(File.dirname(__FILE__) + '/loader')
 
-require 'webrat'
-include Webrat::Methods
-include Webrat::Matchers
- 
 Webrat.configure do |config|
   config.mode = :mechanize
+  config.application_framework = :external
 end
+
+World do
+  Webrat::Session.new(Webrat::MechanizeAdapter.new)
+end
+
+World(Webrat::Matchers)
+World(Spec::Matchers)
